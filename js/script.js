106830c293e3f6eed8ae57e0b207dd9c1fe96b9d
Paul Kim <iam@paulkim.me>
2024-10-09 10:30:52 -0400

    Push

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let hackclubLink = "https://hackclub.com";
 let counterspellLink = "https://counterspell.hackclub.com";
 let learnMoreLink = "https://mailchimp.com/resources/404-page/";
-let whoWeAre = "https://paulkim.me";
+let JinwooKim = "https://kimjinwoo.me/"
+let PaulKim = "https://paulkim.me";
