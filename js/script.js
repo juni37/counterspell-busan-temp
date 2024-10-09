5a2b175672f578b662dc39c85648f0f6dae6b04e
Paul Kim <iam@paulkim.me>
2024-10-09 10:08:40 -0400

    Temp Commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let hackclubLink = "https://hackclub.com";
+let counterspellLink = "https://counterspell.hackclub.com";
+let learnMoreLink = "https://mailchimp.com/resources/404-page/";
+let whoWeAre = "https://paulkim.me";
